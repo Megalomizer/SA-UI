356cfa100092003e9b283acdb8ebb8a2d07fb51d
lfunk <levifunk01@gmail.com>
2025-06-25 18:47:24 +0200

    Updated main page

---
+++ src/components/navbar.tsx
@@ -0,0 +1,16 @@
+export default function NavBar() {
+    return (
+        <>
+            <nav className="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">Navbar</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false"
+                            aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                </div>
+            </nav>
+        </>
+    );
+}
